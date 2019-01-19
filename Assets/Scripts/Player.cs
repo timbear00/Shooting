bf47df8d4fc567bbb3cf98c06868e05f105df1f7
timbear00 <timbear00@naver.com>
2019-01-19 16:55:36 +0900

    SampleScene에서 Player.cs 스크립트 추가. PlayerMove.cs 수정

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public static int hp = 100;
+
+    public Text playerHp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHp.text = "HP : " + hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if( collision.gameObject.tag == "enemy" )
+        {
+            hp -= 5;
+            playerHp.text = "HP : " + hp;
+        }
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     public float speed;
 
