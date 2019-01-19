7f98e525d96cfc7bb0a0ec0f933b9a17e1580d8d
timbear00 <timbear00@naver.com>
2019-01-19 15:32:11 +0900

    player 오브젝트 추가 피카츄 이미지 추가. Move.cs 스크립트 추가.; w,a,s,d와 화살표 키를 이용하여 플레이어를 움직이는 스크립트.

---
+++ Assets/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKey( KeyCode.W ) || Input.GetKey( KeyCode.UpArrow ) )
+        {
+            transform.position += new Vector3(0, 5 * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey( KeyCode.DownArrow ) )
+        {
+            transform.position += new Vector3(0, -5 * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey( KeyCode.LeftArrow ) )
+        {
+            transform.position += new Vector3(-5 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey( KeyCode.RightArrow ) )
+        {
+            transform.position += new Vector3(5 * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
